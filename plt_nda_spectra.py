304b175c8e4ff4563bfe6fbee7222f9b3a8b15cb
Shilpi Bhunia <80779369+Shilpi96@users.noreply.github.com>
2026-04-24 15:41:20 +0200

    Update plt_nda_spectra.py

--- plt_nda_spectra.py
+++ plt_nda_spectra.py
@@ -1,3 +1,5 @@
+### plotting Stokes I and V
+
 from astropy.io import fits
 import numpy as np
 from astropy.time import Time
@@ -17,7 +19,7 @@ hdul = fits.open(fitsfile)
 # =========================
 # 2. GET FREQUENCY
 # =========================
-freq = hdul[1].data[0][0]   # shape: (n_freq,)
+freq = hdul[1].data[0][0]
 
 # =========================
 # 3. GET TIME ARRAY
@@ -35,6 +37,84 @@ times = np.array(times)
 # =========================
 startt = datetime(2025, 3, 26, 9, 21)
 endt   = datetime(2025, 3, 26, 9, 51)
+
+startid = np.argmin(np.abs(times - startt))
+endid   = np.argmin(np.abs(times - endt))
+
+Times = times[startid:endid]
+Data  = hdul[2].data[startid:endid]
+
+print("Selected time range:", Times[0], "to", Times[-1])
+
+# =========================
+# 5. EXTRACT LL & RR
+# =========================
+ldata = np.array([row[1][:, 0] for row in Data])
+rdata = np.array([row[1][:, 1] for row in Data])
+
+print("ldata shape:", ldata.shape)
+print("rdata shape:", rdata.shape)
+
+# =========================
+# 6. COMPUTE STOKES I & V
+# =========================
+I = (ldata + rdata) / 2.0
+V = (rdata - ldata) / 2.0
+
+# =========================
+# 7. DATA percentile for plotting
+# =========================
+# For I (log scale)
+I[I <= 0] = np.nan
+vmin_I = np.nanpercentile(I, 5)
+vmax_I = np.nanpercentile(I, 95)
+
+# For V (symmetric scale)
+vmax_V = np.nanpercentile(np.abs(V), 70)
+
+# =========================
+# 8. PLOT BOTH
+# =========================
+time_nums = mdates.date2num(Times)
+
+fig, axs = plt.subplots(2, 1, figsize=(12, 8), sharex=True)
+
+# ---- Stokes I ----
+im1 = axs[0].pcolormesh(time_nums, freq, I.T,
+                        shading='auto',
+                        cmap='viridis',
+                        norm=LogNorm(vmin=vmin_I, vmax=vmax_I))
+
+axs[0].set_ylabel('Frequency (MHz)')
+axs[0].set_title('NDA Dynamic Spectrum (Stokes I)')
+axs[0].invert_yaxis()
+
+cbar1 = plt.colorbar(im1, ax=axs[0])
+cbar1.set_label('Intensity (log scale)')
+
+# ---- Stokes V ----
+im2 = axs[1].pcolormesh(time_nums, freq, V.T,
+                        shading='auto',
+                        cmap='seismic',
+                        vmin=-vmax_V,
+                        vmax=+vmax_V)
+
+axs[1].set_ylabel('Frequency (MHz)')
+axs[1].set_title('NDA Dynamic Spectrum (Stokes V)')
+axs[1].invert_yaxis()
+
+cbar2 = plt.colorbar(im2, ax=axs[1])
+cbar2.set_label('Stokes V')
+
+# ---- Time axis formatting ----
+axs[1].xaxis_date()
+axs[1].xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+axs[1].set_xlabel('Time (UT)')
+
+plt.tight_layout()
+plt.show()
+
+print("Done!")endt   = datetime(2025, 3, 26, 9, 51)
 
 startid = np.argmin(np.abs(times - startt))
 endid   = np.argmin(np.abs(times - endt))
